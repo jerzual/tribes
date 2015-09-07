474d308d8e1fc58104296e7d4b62da3907be2246
Benjamin Legrand <benjilegnard@protonmail.com>
2015-09-07 20:46:37 +0200

    added gulp tasks

---
+++ www/javascripts/engine/game.js
@@ -0,0 +1,18 @@
+var RNG = require('rng-js');
+
+var Game = {
+    init : function(){
+
+    },
+    animate : function(){
+
+    },
+    /**
+     * main game loop
+     */
+    loop : function(){
+
+    }
+};
+
+module.exports.Game = Game;
