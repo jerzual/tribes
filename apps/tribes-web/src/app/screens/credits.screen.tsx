4894062fab759677e9ce4e9e0c2c558e60488282
Benjamin Legrand <benjilegnard@protonmail.com>
2026-02-27 21:55:21 +0100

    chore: upgrade to React 19

--- apps/tribes-web/src/app/screens/credits.screen.tsx
+++ apps/tribes-web/src/app/screens/credits.screen.tsx
@@ -1,11 +1,7 @@
-import React, { Component } from 'react';
-
-export class CreditsScreen extends Component {
-  public render() {
-    return (
-      <div>
-        <h1>Credits</h1>
-      </div>
-    );
-  }
+export function CreditsScreen() {
+  return (
+    <div>
+      <h1>Credits</h1>
+    </div>
+  );
 }
--- apps/tribes-web/src/app/three/tile.mesh.tsx
+++ apps/tribes-web/src/app/three/tile.mesh.tsx
@@ -4,14 +4,18 @@ import { Mesh, Vector3 } from 'three';
 
 export const Tile: FunctionComponent<{ position: number[] }> = (props) => {
   // This reference will give us direct access to the mesh
-  const mesh: React.MutableRefObject<Mesh> = useRef();
+  const mesh = useRef<Mesh>(null);
 
   // Set up state for the hovered and active state
   const [isHovered, setHover] = useState(false);
   const [isActive, setActive] = useState(false);
 
   // Rotate mesh every frame, this is outside of React without overhead
-  useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01));
+  useFrame(() => {
+    if (mesh.current) {
+      mesh.current.rotation.x = mesh.current.rotation.y += 0.01;
+    }
+  });
 
   return (
     <mesh
@@ -23,7 +27,7 @@ export const Tile: FunctionComponent<{ position: number[] }> = (props) => {
       onPointerOver={(e) => setHover(true)}
       onPointerOut={(e) => setHover(false)}
     >
-      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+      <boxGeometry attach="geometry" args={[1, 1, 1]} />
       <meshStandardMaterial
         attach="material"
         color={isHovered ? 'hotpink' : 'orange'}
